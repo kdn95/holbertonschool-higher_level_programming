19f05f83548ea608ee59713e5078759615cc78bb
kdn95 <khangdn95@gmail.com>
2024-05-07 14:36:07 +1000

    updated task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,5 +2,7 @@
 def islower(c):
     if c >= chr(97) and c <= chr(123):
         return True
-    elif c <= chr(96):
+    elif c >= chr(65) and c <= chr(90):
         return False
+    else:
+        return True
