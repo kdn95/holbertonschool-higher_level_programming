84c887126134ceb4ad2501487dd0f25b300506aa
kdn95 <khangdn95@gmail.com>
2024-05-14 09:52:41 +1000

    updated task 0 one more

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-     for digit in my_list:
-        if isinstance(digit, int):
-            print("{:d}".format(digit))
-        else:
-            break
+    for digit in my_list:
+        print("{:d}".format(digit))
