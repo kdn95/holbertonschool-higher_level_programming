3859c3988521e0eb02ea06e933d66e28f943973c
kdn95 <khangdn95@gmail.com>
2024-05-14 10:11:26 +1000

    added :d in .format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for in_mat in matrix:
         for num in in_mat:
-            print("{}".format(num), end="\t")
-        print("")
+            print("{:d}".format(num), end="\t")
+        print("{}".format(''))
