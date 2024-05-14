39373e64f6c213a82331c2527b922b8a217f1e1e
kdn95 <khangdn95@gmail.com>
2024-05-14 13:19:23 +1000

    task 6 revised to recursive function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for in_mat in matrix:
-        for num in in_mat:
-            print("{:d}".format(num), end=" ")
-        print("{}".format(""))
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))
