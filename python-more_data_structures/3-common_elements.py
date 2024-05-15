cc6ddb9eb8be94a5970ec8ba197c5e6abb808e95
kdn95 <khangdn95@gmail.com>
2024-05-15 12:57:56 +1000

    sync all main files

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,7 +2,7 @@
 def common_elements(set_1, set_2):
     list_1 = list(set_1)
     list_2 = list(set_2)
-    common_list= []
+    common_list = []
     for duplicate in list_1:
         if duplicate in list_2:
             common_list.append(duplicate)
