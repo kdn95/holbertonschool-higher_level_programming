0f6853ba864f3ff347d87f945eb233920f6ccb10
kdn95 <khangdn95@gmail.com>
2024-05-16 15:21:26 +1000

    updated task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
+    if not isinstance(roman_string, str):
+        return 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     num_list = []
     for letter in roman_string:
         if (letter in roman):
             num_list.append(roman[letter])
         else:
-            return None
+            return 0
     for i in range(len(num_list) - 1):
         if num_list[i] < num_list[i + 1]:
             num_list[i] = -num_list[i]
     return sum(num_list)
-        
