1d742304d4c94e90af8c9493b820c3466b618ff2
kdn95 <khangdn95@gmail.com>
2024-05-23 15:57:34 +1000

    fixed task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@ This function will add 2 integers together
 
 def add_integer(a, b=98):
     """
-    Thsi function returns the sum of the args provided.
+    This function returns the sum of the args provided.
 
     Args:
     a (int/float) : first
@@ -15,10 +15,10 @@ def add_integer(a, b=98):
     Return:
         int: sum of a & b
     """
-    if isinstance(a, (int, float)) or a is None:
+    if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    if isinstance(b, (int, float)) or b is None:
+    if b is None or not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return a + b
+    return int(a) + int(b)
