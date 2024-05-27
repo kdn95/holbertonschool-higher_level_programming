4cc105fc49cb0c882e9f180c2f3a7ad771dec999
kdn95 <khangdn95@gmail.com>
2024-05-27 17:44:50 +1000

    added task 0

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module for Rectangle class"""
+
+
+class Rectangle:
+    """
+    Rectangle class with attributes
+    """
+
+    def __init__(self, width=0, height=0)
+        """ initialization """
+        self.width = width
+        self.height = height
+
+    
