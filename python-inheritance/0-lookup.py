c94e0b61d92b526b7ae63f7d9faac38cc30415b0
kdn95 <khangdn95@gmail.com>
2024-05-28 18:00:41 +1000

    added task 0 again

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A function that returns a list of attributes &
+methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    Return a list object.
+    """
+    return dir(obj)
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A class called MyList that inherits from list
+"""
+
+class MyList(list):
+    """
+    Subclass of built-in list
+    """
+    def print_sorted(self):
+        """
+        Prints list of elements in ascending order
+        """
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    my_list = MyList([3, 1, 4, 5, 2])
+    my_list.print_sorted()
