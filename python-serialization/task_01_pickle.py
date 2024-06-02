1165ba30123ddf9ec0a0dff78d571a3167f94e50
kdn95 <khangdn95@gmail.com>
2024-06-03 08:45:33 +1000

    updated task 1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -4,11 +4,17 @@ Module for serialization and deserialization
 using Pickle
 """
 import pickle
+import sys
 
 
 class CustomObject:
     """CustomObject Class"""
-    def __init__(self, name, age, is_student):
+
+    name = ""
+    age = 0
+    is_student = False
+
+    def __init__(self, name="", age=0, is_student=False):
         self.name = name
         self.age = age
         self.is_student = is_student
@@ -21,11 +27,23 @@ class CustomObject:
 
     def serialize(self, filename):
         """To serialize current instance of obj"""
-        with open(filename, "wb") as w_file:
-            pickle.dump(self, w_file)
+        if not filename:
+            print("Invalid filename specified!")
+            sys.exit()
+        try:
+            with open(filename, "wb") as w_file:
+                pickle.dump(self, w_file)
+        except FileNotFoundError:
+            print("File not found")
 
     @classmethod
     def deserialize(cls, filename):
         """To load and return instance of CustomObject"""
-        with open(filename, "rb") as r_file:
-            return pickle.load(r_file)
+        if not filename:
+            print("Invalid filename specified!")
+            sys.exit()
+        try:
+            with open(filename, "rb") as r_file:
+                return pickle.load(r_file)
+        except FileNotFoundError:
+            print("File not found")
