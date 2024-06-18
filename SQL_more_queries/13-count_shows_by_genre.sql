4c09e4a056a1418d2ff199ec1d505203928efc1d
kdn95 <khangdn95@gmail.com>
2024-06-18 19:35:03 +1000

    further corrections

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,7 +9,6 @@
 SELECT name AS genre, COUNT(show_id) AS number_of_shows
     FROM tv_genres
         INNER JOIN tv_show_genres
-            ON tv_genres.id = tv_show_genres.show_id
+            ON tv_genres.id = tv_show_genres.genre_id
                 GROUP BY name
-                    HAVING COUNT(*) > 0
-                        ORDER BY number_of_shows DESC;
+                    ORDER BY number_of_shows DESC;
--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,17 +3,11 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
-SELECT tv_genres.name
+SELECT DISTINCT tv_genres.name
     FROM tv_genres
-        -- Looking for inner join between genre_id & id in tv_genres
         JOIN tv_show_genres
             ON tv_genres.id = tv_show_genres.genre_id
-                -- Making tv_show_genres.show_id equal to tv_shows.id
-                WHERE tv_show_genres.show_id =
-                    (SELECT id
-                        FROM tv_shows
-                            -- Specifically matching to "Dexter"
-                            WHERE tv_shows.title="Dexter"
-                    )
-                    -- Ordered by genre name in ASC order
+        JOIN tv_shows
+            ON tv_show_genres.show_id = tv_shows.id
+                WHERE tv_shows.title = "Dexter"
                     ORDER BY tv_genres.name ASC;
