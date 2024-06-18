cdf426adea23df37f8b9fe517a8bf6dd1b561005
kdn95 <khangdn95@gmail.com>
2024-06-18 12:49:52 +1000

    task 0-3 added

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create user user_0d_1
+-- ps = user_0d_1_pwd
+-- if already exists, script shouldn't fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Provide all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Apply changes
+FLUSH PRIVILEGES;
