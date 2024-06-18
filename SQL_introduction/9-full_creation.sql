683de26dd34eb768bfd0e01ad8b720798c65c9f0
kdn95 <khangdn95@gmail.com>
2024-06-18 10:32:02 +1000

    further updates

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,13 @@
 -- Create second_table in hbtn_0c_0
 -- id INT, name VARCHAR(256), score INT
-CREATE TABLE IF NOT EXISTS second_table('id' INT PRIMARY KEY, 'name' VARCHAR(256), 'score' INT);
-INSERT INTO second_table ('id', 'name', 'score')
-VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+CREATE TABLE IF NOT EXISTS second_table(
+    `id` INT PRIMARY KEY,
+    `name` VARCHAR(256),
+    `score` INT
+);
+
+INSERT INTO second_table (`id`, `name`, `score`) VALUES
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
