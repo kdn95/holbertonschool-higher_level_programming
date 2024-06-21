920109412594e96cf299aaab2309d5e95571b582
kdn95 <khangdn95@gmail.com>
2024-06-21 14:21:01 +1000

    added task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""List all states from hbtn_0e_0_usa db"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+#Establish connection using MySQLdb
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+    )
+
+#Create cursor object to interact with db by executing SQL queries
+cur = db.cursor()
+cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+# fetchall() retrieves all rows returned from SQL query in execute()
+rows = cur.fetchall()
+# Returns list of tuples. Each tuple is a row from the result set of the query
+for row in rows:
+    print("({0}, {1})".format(row[0], row[1]))
