d057f7e950fd9d44d6cd9fde9acc7c9f0b3f2d9f
kdn95 <khangdn95@gmail.com>
2024-06-21 14:40:09 +1000

    added task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""List all states that start with the uppercase letter N"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+#Establish connection using MySQLdb
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+    )
+
+#Create cursor object to interact with db by executing SQL queries
+cur = db.cursor()
+cur.execute("""SELECT *
+            FROM states
+            WHERE name LIKE 'N%'
+            ORDER BY id ASC
+            LIMIT 2""")
+# fetchall() retrieves all rows returned from SQL query in execute()
+rows = cur.fetchall()
+# Returns list of tuples. Each tuple is a row from the result set of the query
+for row in rows:
+    print("({0}, {1})".format(row[0], row[1]))
