6441a0b0669300b70413495418476afcc1ac92e2
kdn95 <khangdn95@gmail.com>
2024-06-21 14:46:17 +1000

    indentation updates

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,23 +7,23 @@ if __name__ == "__main__":
     import sys
 
 #Establish connection using MySQLdb
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3]
     )
 
 #Create cursor object to interact with db by executing SQL queries
-cur = db.cursor()
-cur.execute("""SELECT *
-            FROM states
-            WHERE name LIKE 'N%'
-            ORDER BY id ASC
-            LIMIT 2""")
+    cur = db.cursor()
+    cur.execute("""SELECT *
+                FROM states
+                WHERE name LIKE 'N%'
+                ORDER BY id ASC
+                LIMIT 2""")
 # fetchall() retrieves all rows returned from SQL query in execute()
-rows = cur.fetchall()
+    rows = cur.fetchall()
 # Returns list of tuples. Each tuple is a row from the result set of the query
-for row in rows:
-    print("({0}, {1})".format(row[0], row[1]))
+    for row in rows:
+        print("({0}, {1})".format(row[0], row[1]))
