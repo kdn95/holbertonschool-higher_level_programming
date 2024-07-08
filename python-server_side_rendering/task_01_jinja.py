e2b9842bcb94d0628eb60c80ed5eb5d4c3ad7b2e
kdn95 <khangdn95@gmail.com>
2024-07-08 18:54:45 +1000

    added task 01

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Basic Flask App"""
+from flask import Flask, render_template
+# Create an instance for Flask
+app = Flask(__name__)
+# Create home page
+@app.route('/')
+def home():
+    """ Use render_template function to return index.html """
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    """ Use render_template function to return about.html """
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    """ Use render_template function to return contact.html """
+    return render_template('contact.html')
+
+# Conditional check if script is being directly executed
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
